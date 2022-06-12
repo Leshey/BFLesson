395d9987a1d3856bc23696d9710f08e237771bd1
Vladislav Kovalchuk <leshey95@gmail.com>
2022-06-12 19:28:47 +0300

    Added new functional for future BF code debugger

--- BrainFuck/Program.cs
+++ BrainFuck/Program.cs
@@ -5,13 +5,18 @@ using BrainFuck.Menus;
 
 namespace BrainFuck;
 
+using System.Text;  //убрать позже
+
 public static class Program
 {
     public static void Main()
     {
-        var menu = BuildMenu();
+        var repository = new Repository("++++++++10--------20++++++++30--------40++++++++50--------60++++++++70--------80++++++++90-------100+++++++110-------120+++++++130-------140+++++++150-------160+++++++170-------180+++++++190-------200");
+        var bfDebugger = new BfDebuggerMenu(repository);
+        bfDebugger.DebugMenu();
 
-        menu.RunMenu();
+        //var menu = BuildMenu();
+        //menu.RunMenu();
     }
 
     private static IMenu BuildMenu()
@@ -22,15 +27,99 @@ public static class Program
         var bfInterpretation = new BfInterpretation(inputOutput);
         menuBuilder
             .AddNewMenuLine(
-                new MenuLine("Запустить стандартную программу", 
+                new MenuLine("Запустить стандартную программу",
                 new DefaultBrainFuckCommand(bfInterpretation)))
             .AddNewMenuLine(
-                new MenuLine("Ввести программу вручную", 
+                new MenuLine("Ввести программу вручную",
                 new HandelInputBfCodeAndRunCommand(bfInterpretation)))
             .AddNewMenuLine(
-                new MenuLine("Выйти", 
+                new MenuLine("Дебаг меню",
+                new ExitCommand(menuBuilder.ExitToken)))
+            .AddNewMenuLine(
+                new MenuLine("Выйти",
                 new ExitCommand(menuBuilder.ExitToken)));
 
         return menuBuilder.Build();
+    }
+
+    public class BfDebuggerMenu
+    {
+        private readonly Repository _dataFromRepository;
+        private char _cursor = '^';
+        private string _BFCodeString;
+        private int _cursorIndex = 0;
+
+        public BfDebuggerMenu(Repository dataFromRepository)
+        {
+            _dataFromRepository = dataFromRepository;
+            _BFCodeString = dataFromRepository.Program;
+        }
+
+        public void PrintBFCode(string programString)
+        {
+            int consoleWidth = Console.WindowWidth;
+            if (programString.Length > consoleWidth) 
+            {
+                for (int i = 0; i < consoleWidth; i++) 
+                {
+                    Console.Write(programString[i]);
+                }
+            }
+            else
+            {
+                Console.Write(programString);
+            }
+        }
+
+        public string DeleteFirstCharInString(string programString)
+        {
+            return _BFCodeString = programString.Remove(0, 1);
+        }
+
+        public void PrintCursorAndBFCode() 
+        {
+            var consoleCursorWrapper = new ConsoleCursorWrapper();
+            int consoleWidthHalf = Console.WindowWidth / 2;
+            consoleCursorWrapper.SetCursorPosition(0, 1);
+            Console.Write(_cursor);
+            while (true)
+            {
+                ConsoleKeyInfo consoleKeyInfo = Console.ReadKey(true);
+                if (consoleKeyInfo.Key == ConsoleKey.N)
+                {
+                    if (_cursorIndex < consoleWidthHalf)
+                    {
+                        Console.Write("\b ");
+                        Console.Write(_cursor);
+                    }
+                    else if(_cursorIndex >= consoleWidthHalf && _cursorIndex < _dataFromRepository.Program.Length - consoleWidthHalf)
+                    {
+                        consoleCursorWrapper.SetCursorPosition(0, 0);
+                        PrintBFCode(DeleteFirstCharInString(_BFCodeString));
+                    }
+                    else if (_cursorIndex == _dataFromRepository.Program.Length - consoleWidthHalf)
+                    {
+                        consoleCursorWrapper.SetCursorPosition(consoleWidthHalf, 1);
+                        Console.Write("\b ");
+                        Console.Write(_cursor);
+                    }
+                    else if (_cursorIndex >= _dataFromRepository.Program.Length - consoleWidthHalf)
+                    {
+                        Console.Write("\b ");
+                        Console.Write(_cursor);
+                    }
+                    _cursorIndex += 1;
+                }
+            }
+        }
+
+        public void DebugMenu() 
+        {
+            PrintBFCode(_dataFromRepository.Program);
+            while (true) 
+            {
+                PrintCursorAndBFCode();
+            }
+        }
     }
 }
--- BrainFuckTestProject/UnitTest1.cs
+++ BrainFuckTestProject/UnitTest1.cs
@@ -1,298 +1,228 @@
-using System.IO;
-using System.Text;
-using BrainFuck;
-using Xunit;
+using BrainFuck.BF;
+using BrainFuck.IO;
+using BrainFuck.Interfaces.IO;
+using BrainFuck.Interfaces.BF;
+using BrainFuck.Interfaces.Menus;
 using Moq;
+using Xunit;
+using System.IO;
 
-namespace BrainFuckTestProject
+
+namespace BrainFuck;
+
+public class DataOperationTest
 {
-    public class DataOperationTest
+    [Theory]
+    [InlineData(1)]
+    [InlineData(0)]
+    [InlineData(69)]
+    public void NextCharValueTest(int newValue)
     {
-        //[Theory]
-        //[InlineData(0)]
-        //[InlineData(69)]
-        //[InlineData(121)]
-        //public void NextCellTest(int newCurrent)
-        //{
-        //    // arrange
-        //    var repository = new Repository();
-        //    var testTextWriter = new TestTextWriter();
-        //    var testTextReader = new TestTextReader();
-        //    var inputOutput = new InputOutput(testTextReader, testTextWriter);
-        //    var brainFuckFunction = new BrainFuckFunction(repository, inputOutput);
+        // arrange
+        var repository = new Repository("");
+        var mockInputOutput = new Mock<IInputOutput>();
+        var brainFuckFunction = new BrainFuckFunction(repository, mockInputOutput.Object);
 
-        //    var expectedCurrent1 = newCurrent + 1;
-        //    repository.Current = newCurrent;
+        var expectedCurrent = newValue + 1;
+        repository.Memory[repository.Current] = (char)newValue;
 
-        //    // act
-        //    brainFuckFunction.NextCell();
-        //    var actual1 = repository.Current;
+        // act
+        brainFuckFunction.NextCharValue();
+        var actual = repository.Memory[repository.Current];
 
-
-        //    // assert
-
-        //    Assert.Equal(expectedCurrent1, actual1);
-
-        //}
-
-        //[Theory]
-        //[InlineData(5)]
-        //[InlineData(4)]
-        //[InlineData(13121)]
-        //public void PreviusCellTest(int newCurrent)
-        //{
-        //    // arrange
-        //    var repository = new Repository();
-        //    var testTextWriter = new TestTextWriter();
-        //    var testTextReader = new TestTextReader();
-        //    var inputOutput = new InputOutput(testTextReader, testTextWriter);
-        //    var brainFuckFunction = new BrainFuckFunction(repository, inputOutput);
-
-        //    var expectedCurrent1 = newCurrent - 1;
-        //    repository.Current = newCurrent;
-
-        //    // act
-        //    brainFuckFunction.PreviousCell();
-        //    var actual1 = repository.Current;
-        //    // assert
-
-        //    Assert.Equal(expectedCurrent1, actual1);
-        //}
-
-        //[Theory]
-        //[InlineData(1)]
-        //[InlineData(2)]
-        //[InlineData(3)]
-        //public void NextCharValueTest(int newCurrent)
-        //{
-        //    // arrange
-        //    var repository = new Repository();
-        //    var testTextWriter = new TestTextWriter();
-        //    var testTextReader = new TestTextReader();
-        //    var inputOutput = new InputOutput(testTextReader, testTextWriter);
-        //    var brainFuckFunction = new BrainFuckFunction(repository, inputOutput);
-
-        //    var expectedCurrent1 = (char)newCurrent + 1;
-        //    repository.Memory[repository.Current] = (char)newCurrent;
-
-        //    // act
-        //    brainFuckFunction.NextCharValue();
-        //    var actual1 = repository.Memory[repository.Current];
-
-        //    // assert
-        //    Assert.Equal(expectedCurrent1, actual1);
-        //}
-
-        //[Theory]
-        //[InlineData(33)]
-        //[InlineData(23)]
-        //[InlineData(44)]
-        //public void PreviousCharValueTest(int newCurrent)
-        //{
-        //    // arrange
-        //    var repository = new Repository();
-        //    var testTextWriter = new TestTextWriter();
-        //    var testTextReader = new TestTextReader();
-        //    var inputOutput = new InputOutput(testTextReader, testTextWriter);
-        //    var brainFuckFunction = new BrainFuckFunction(repository, inputOutput);
-
-        //    var expectedCurrent1 = (char)newCurrent - 1;
-        //    repository.Memory[repository.Current] = (char)newCurrent;
-
-        //    // act
-
-        //    brainFuckFunction.PreviousCharValue();
-        //    var actual1 = repository.Memory[repository.Current];
-
-        //    // assert
-        //    Assert.Equal(expectedCurrent1, actual1);
-        //}
-
-
-        //[Theory]
-        //[InlineData("[]", 1)]
-        //[InlineData("[++++[++++++[][][][][][][][][][][]][+++++++][][][][]][][][][][][][][]++]+++++]", 52)]
-        //[InlineData("[++++[++++++++]++++++++[++++[[[+++++++++++[+++++++]]]--------------]]++++]", 73)]
-        //public void IfZeroNextTest(string newCurrentProgram, int expectedCurrent)
-        //{
-        //    // arrange
-        //    var repository = new Repository();
-        //    var testTextWriter = new TestTextWriter();
-        //    var testTextReader = new TestTextReader();
-        //    var inputOutput = new InputOutput(testTextReader, testTextWriter);
-        //    var brainFuckFunction = new BrainFuckFunction(repository, inputOutput);
-        //    repository.Memory[repository.Current] = (char)0;
-        //    repository.Program = newCurrentProgram;
-        //    var expectedCurrent1 = expectedCurrent;
-        //    var PositionNumber = 0;
-        //    // act
-
-        //    var actual1 = brainFuckFunction.IfZeroNext(PositionNumber, repository.Program);
-        //    // assert
-        //    Assert.Equal(expectedCurrent1, actual1);
-
-        //}
-
-        //[Theory]
-        //[InlineData("[++++[++++++++]+++++++]", 22)]
-        //[InlineData("[[[]]]", 5)]
-        //[InlineData("[++++[++++++++]++++++++[++++[[[+++++++++++[+++++++]]]--------------]]++++]", 73)]
-        //public void IfNoZeroBackTest(string newCurrentProgram, int newPositionNumber)
-        //{
-        //    // arrange
-        //    var repository = new Repository();
-        //    var testTextWriter = new TestTextWriter();
-        //    var testTextReader = new TestTextReader();
-        //    var inputOutput = new InputOutput(testTextReader, testTextWriter);
-        //    var brainFuckFunction = new BrainFuckFunction(repository, inputOutput);
-        //    repository.Memory[repository.Current] = (char)1;
-        //    repository.Program = newCurrentProgram;
-        //    var expectedCurrent1 = 0;
-        //    var positionNumber = newPositionNumber;
-
-        //    // act
-
-        //    var actual1 = brainFuckFunction.IfNoZeroBack(newPositionNumber, repository.Program);
-
-        //    // assert
-        //    Assert.Equal(expectedCurrent1, actual1);
-        //}
-
-        //[Theory]
-        //[InlineData("}")]
-        //[InlineData("<")]
-        //[InlineData("/")] 
-        //public void DisplayCellValueTest(string expected)
-        //{
-        //    // arrange
-        //    var mockTextWriter = new Mock<TextWriter>();
-        //    var called = false;
-        //    mockTextWriter.Setup(x => x.Write(expected)).Callback(() => called = true);
-
-        //    var repository = new Repository();
-        //    var testTextReader = new TestTextReader();
-
-        //    var inputOutput = new InputOutput(testTextReader, mockTextWriter.Object);
-        //    var brainFuckFunction = new BrainFuckFunction(repository, inputOutput);
-
-        //    repository.Memory[0] = expected[0];
-
-        //    // act
-        //    brainFuckFunction.DisplayCellValue();
-
-
-        //    // assert
-        //    Assert.True(called);
-        //}
-
-        //[Theory]
-        //[InlineData('}', "}")]
-        //[InlineData('+', "+")]
-        //[InlineData('_', "_")]
-        //public void InputValueInCellTest(char expectedCurrent, string value)
-        //{
-        //    // arrange
-
-        //    var mockTextReader = new Mock<TextReader>();
-        //    mockTextReader.Setup(x => x.ReadLine()).Returns(value);
-
-        //    var repository = new Repository();
-        //    var testTextWriter = new TestTextWriter();
-        //    var inputOutput = new InputOutput(mockTextReader.Object, testTextWriter); //Object õðàíèò â ñåáå ðåàëèçàöèþ
-        //    var brainFuckFunction = new BrainFuckFunction(repository, inputOutput);
-
-
-        //    // act
-
-        //    brainFuckFunction.InputValueInCell();
-        //    var actual = repository.Memory[0];
-        //    // assert
-        //    mockTextReader.Verify(x => x.ReadLine(), Times.Once);
-        //    Assert.Equal(expectedCurrent, actual);
-
-        //}
-
-        //[Theory]
-        //[InlineData("+", "NextCharValue")]
-        //[InlineData("-", "PreviousCharValue")]
-        //[InlineData(".", "DisplayCellValue")]
-        //[InlineData(">", "NextCell")]
-        //[InlineData("<", "PreviousCell")]
-        //[InlineData(",", "InputValueInCell")]
-        //[InlineData("[", "IfZeroNext")]
-        //[InlineData("]", "IfNoZeroBack")]
-        //public void EnumÑodeBrainFuckTest(string brainFuckCode, string expectedName)
-        //{
-        //    // arrange
-        //    var mockBrainFuckFunction = new Mock<IBrainFuckFunction>();
-
-        //    var actualName = "õóé";
-
-        //    mockBrainFuckFunction.Setup(x => x.NextCharValue()).Callback(() => actualName = "NextCharValue");
-        //    mockBrainFuckFunction.Setup(x => x.PreviousCharValue()).Callback(() => actualName = "PreviousCharValue");
-        //    mockBrainFuckFunction.Setup(x => x.DisplayCellValue()).Callback(() => actualName = "DisplayCellValue");
-        //    mockBrainFuckFunction.Setup(x => x.NextCell()).Callback(() => actualName = "NextCell");
-        //    mockBrainFuckFunction.Setup(x => x.PreviousCell()).Callback(() => actualName = "PreviousCell");
-        //    mockBrainFuckFunction.Setup(x => x.InputValueInCell()).Callback(() => actualName = "InputValueInCell");
-        //    mockBrainFuckFunction.Setup(x => x.IfZeroNext(0, brainFuckCode)).Callback(() => actualName = "IfZeroNext");
-        //    mockBrainFuckFunction.Setup(x => x.IfNoZeroBack(0, brainFuckCode)).Callback(() => actualName = "IfNoZeroBack");
-
-        //    var dataOperations = new DataOperations(mockBrainFuckFunction.Object);
-
-
-
-        //    // act
-        //    dataOperations.EnumÑodeBrainFuck(brainFuckCode);
-
-
-
-        //    // assert
-
-        //    Assert.Equal(expectedName, actualName);
-        //}
-
-        //public class TestTextReader : TextReader
-        //{
-        //    private readonly string _input;
-
-        //    public TestTextReader(string input)
-        //    {
-        //        _input = input;
-        //    }
-        //    public TestTextReader()
-        //    { 
-
-        //    }
-        //    public override string ReadLine()
-        //    {
-        //        return _input;
-        //    }
-        //}
-
-        //public class TestTextWriter : TextWriter
-        //{
-        //    private string _output;
-
-        //    public string OutputHeh => _output; 
-
-        //    public TestTextWriter(string output)
-        //    {
-        //        _output = output;
-
-        //    }
-        //    public TestTextWriter() // êîíñòðóêòîð, êîòîðûé ïîçâîëÿåò ñîçäàòü ýêçåìïëÿð êëàññà áåç àãðóìåíòà
-        //    {
-
-        //    }
-        //    public override Encoding Encoding => Encoding.UTF8; //ïîçâîëÿåò âðàéòåðó ïîíÿòü ÷òî íàïèñàòü
-
-        //    public override void Write(string output)
-        //    {
-        //        _output = output;
-        //    }
-        //}
-
+        // assert
+        Assert.Equal(expectedCurrent, actual);
     }
-    
+
+    [Theory]
+    [InlineData(1)]
+    [InlineData(15)]
+    [InlineData(69)]
+    public void PreviousCharValueTest(int newValue)
+    {
+        // arrange
+        var repository = new Repository("");
+        var mockInputOutput = new Mock<IInputOutput>();
+        var brainFuckFunction = new BrainFuckFunction(repository, mockInputOutput.Object);
+
+        var expectedCurrent = newValue - 1;
+        repository.Memory[repository.Current] = (char)newValue;
+
+        // act
+        brainFuckFunction.PreviousCharValue();
+        var actual = repository.Memory[repository.Current];
+
+        // assert
+        Assert.Equal(expectedCurrent, actual);
+    }
+
+    [Theory]
+    [InlineData("}")]
+    [InlineData("<")]
+    [InlineData("/")]
+    public void DisplayCellValueTest(string newValue)
+    {
+        // arrange
+        var repository = new Repository("");
+        var mockCursorWrapper = new Mock<ICursorWrapper>();
+        var mockTextReader = new Mock<TextReader>();
+        var mockTextWriter = new Mock<TextWriter>();
+        var called = false;
+        mockTextWriter.Setup(x => x.Write(newValue)).Callback(() => called = true);
+
+        var inputOutput = new InputOutput(mockTextReader.Object, mockTextWriter.Object, mockCursorWrapper.Object);
+        var brainFuckFunction = new BrainFuckFunction(repository, inputOutput);
+
+        repository.Memory[0] = newValue[0];
+
+        // act
+        brainFuckFunction.DisplayCellValue();
+
+        // assert
+        Assert.True(called);
+    }
+
+    [Theory]
+    [InlineData(0)]
+    [InlineData(69)]
+    [InlineData(121)]
+    public void NextCellTest(int newValue) 
+    {
+        // arrange
+        var repository = new Repository("");
+        var mockInputOutput = new Mock<IInputOutput>();
+        var brainFuckFunction = new BrainFuckFunction(repository, mockInputOutput.Object);
+
+        var expectedCurrent = newValue + 1;
+        repository.Current = newValue;
+
+        // act
+        brainFuckFunction.NextCell();
+        var actual = repository.Current;
+
+        // assert
+        Assert.Equal(expectedCurrent, actual);
+    }
+
+    [Theory]
+    [InlineData(1)]
+    [InlineData(69)]
+    [InlineData(121)]
+    public void PreviousCellTest(int newValue)
+    {
+        // arrange
+        var repository = new Repository("");
+        var mockInputOutput = new Mock<IInputOutput>();
+        var brainFuckFunction = new BrainFuckFunction(repository, mockInputOutput.Object);
+
+        var expectedCurrent = newValue - 1;
+        repository.Current = newValue;
+
+        // act
+        brainFuckFunction.PreviousCell();
+        var actual = repository.Current;
+
+        // assert
+        Assert.Equal(expectedCurrent, actual);
+    }
+
+    [Theory]
+    [InlineData('}', "}")]
+    [InlineData('+', "+")]
+    [InlineData('_', "_")]
+    public void InputValueInCellTest(char expectedValue, string newValue) 
+    {
+        // arrange
+        var repository = new Repository("");
+        var mockCursorWrapper = new Mock<ICursorWrapper>();
+        var mockTextReader = new Mock<TextReader>();
+        mockTextReader.Setup(x => x.ReadLine()).Returns(newValue);
+        var mockTextWriter = new Mock<TextWriter>();
+        
+        var inputOutput = new InputOutput(mockTextReader.Object, mockTextWriter.Object, mockCursorWrapper.Object); //Object õðàíèò â ñåáå ðåàëèçàöèþ
+        var brainFuckFunction = new BrainFuckFunction(repository, inputOutput);
+
+        // act
+        brainFuckFunction.InputValueInCell();
+        var actual = repository.Memory[0];
+
+        // assert
+        mockTextReader.Verify(x => x.ReadLine(), Times.Once);
+        Assert.Equal(expectedValue, actual);
+    }
+
+    [Theory]
+    [InlineData("[]", 1)]
+    [InlineData("+++++[++++[++++++[][][][][][][][][][][]][+++++++][][][][]][][][][][][][][]++]+++++]", 76)]
+    [InlineData("[++++[++++++++]++++++++[++++[[[+++++++++++[+++++++]]]--------------]]++++]", 73)]
+    public void IfZeroNextTest(string newProgram, int expectedValue)
+    {
+        // arrange
+        var repository = new Repository(newProgram);
+        var mockInputOutput = new Mock<IInputOutput>();
+        var brainFuckFunction = new BrainFuckFunction(repository, mockInputOutput.Object);
+        repository.Memory[repository.Current] = (char)0;
+        var PositionNumber = 0;
+       
+        // act
+        var actual = brainFuckFunction.IfZeroNext(PositionNumber, repository.Program);
+        
+        // assert
+        Assert.Equal(expectedValue, actual);
+    }
+
+    [Theory]
+    [InlineData("[++++[++++++++]+++++++]", 22)]
+    [InlineData("[[[]]]", 5)]
+    [InlineData("[++++[++++++++]++++++++[++++[[[+++++++++++[+++++++]]]--------------]]++++]", 73)]
+    public void IfNoZeroBackTest(string newProgram, int newPositionNumber)
+    {
+        // arrange
+        var repository = new Repository(newProgram);
+        var mockInputOutput = new Mock<IInputOutput>();
+        var brainFuckFunction = new BrainFuckFunction(repository, mockInputOutput.Object);
+        repository.Memory[repository.Current] = (char)1;
+        var expectedCurrent1 = 0;
+
+        // act
+        var actual1 = brainFuckFunction.IfNoZeroBack(newPositionNumber, repository.Program);
+
+        // assert
+        Assert.Equal(expectedCurrent1, actual1);
+    }
+
+    [Theory]
+    [InlineData("+", "NextCharValue")]
+    [InlineData("-", "PreviousCharValue")]
+    [InlineData(".", "DisplayCellValue")]
+    [InlineData(">", "NextCell")]
+    [InlineData("<", "PreviousCell")]
+    [InlineData(",", "InputValueInCell")]
+    [InlineData("[", "IfZeroNext")]
+    [InlineData("]", "IfNoZeroBack")]
+    public void EnumÑodeBrainFuckTest(string brainFuckCode, string expectedName)
+    {
+        // arrange
+        var mockBrainFuckFunction = new Mock<IBrainFuckFunction>();
+        var actualName = "invalidName";
+
+        mockBrainFuckFunction.Setup(x => x.NextCharValue()).Callback(() => actualName = "NextCharValue");
+        mockBrainFuckFunction.Setup(x => x.PreviousCharValue()).Callback(() => actualName = "PreviousCharValue");
+        mockBrainFuckFunction.Setup(x => x.DisplayCellValue()).Callback(() => actualName = "DisplayCellValue");
+        mockBrainFuckFunction.Setup(x => x.NextCell()).Callback(() => actualName = "NextCell");
+        mockBrainFuckFunction.Setup(x => x.PreviousCell()).Callback(() => actualName = "PreviousCell");
+        mockBrainFuckFunction.Setup(x => x.InputValueInCell()).Callback(() => actualName = "InputValueInCell");
+        mockBrainFuckFunction.Setup(x => x.IfZeroNext(0, brainFuckCode)).Callback(() => actualName = "IfZeroNext");
+        mockBrainFuckFunction.Setup(x => x.IfNoZeroBack(0, brainFuckCode)).Callback(() => actualName = "IfNoZeroBack");
+
+        var dataOperations = new DataOperations(mockBrainFuckFunction.Object);
+
+        // act
+        dataOperations.EnumÑodeBrainFuck(brainFuckCode);
+
+        // assert
+        Assert.Equal(expectedName, actualName);
+    }
 }
-  
+
+
